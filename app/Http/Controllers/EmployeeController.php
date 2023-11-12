b2206c307a748ee2bccf53dd2d96a5d426bc3e7d
Zilfs <rheinlandcast@gmail.com>
2023-11-12 14:06:51 +0700

    perf : todays order data feature in employee area

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class EmployeeController extends Controller
 {
     public function index()
     {
         $recent_order = Transaction::with('order')->latest()->first();
-        $transaction = Transaction::all()->count();
-        
+        $transaction = Transaction::whereDate('date_time', Carbon::today())->count();
+
         return view('pages.employee.index', [
             'recent_order' => $recent_order,
             'transactions' => $transaction,
